1e4c67b016cf5043cb0a369f1d371cd99499c428
Aditya Tiwari <atiwari411@ucsb.edu>
2026-04-24 21:10:57 -0700

    added in the dynamodb entity key strategy types

---
+++ backend/shared/types/dynamodb.ts
@@ -0,0 +1,28 @@
+/**
+ * documenting key patterns we want to use so handlers easier to implement
+ */
+
+export type EntityType = 
+| "USER"
+| "WORKSPACE"
+| "MEMBERSHIP"
+| "BOARD"
+| "LIST"
+| "TASK"
+| "COMMENT";
+
+/* base shapes for records in dynamo */
+
+export const keys = {
+    userPk: (userId: string) => `USER#${userId}`,
+    workspacePk: (workspaceId: string) => `WORKSPACE#${workspaceId}`,
+    workspaceMetadataSk: () => "METADATA", 
+    membershipSk: (userId: string) => `MEMBER#${userId}`,
+    boardSk: (boardId: string) => `BOARD#${boardId}`,
+    boardPk: (boardId: string) => `BOARD#${boardId}`,
+    listSk: (listId: string) => `LIST#${listId}`,
+    taskSk: (taskId: string) => `TASK#${taskId}`,
+    taskPk: (taskId: string) => `TASK#${taskId}`,
+    commentSk: (createdAt: string, commentId: string) =>
+    `COMMENT#${createdAt}#${commentId}`
+};
